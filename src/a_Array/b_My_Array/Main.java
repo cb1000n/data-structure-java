d6205ebec4f8074e07618b07945f61289465b4e2
cb1000n <48148554+cb1000n@users.noreply.github.com>
2020-08-13 11:18:04 +0800

    Refacotor:重构代码结构

---
+++ src/a_Array/b_My_Array/Main.java
@@ -0,0 +1,14 @@
+package a_Array.b_My_Array;
+
+public class Main {
+    public static void main(String[] args) {
+        Array array1 = new Array(20);
+        System.out.println(array1.getCapacity());
+        System.out.println(array1.getSize());
+        System.out.println(array1.isEmpty());
+        Array array2 = new Array();
+        System.out.println(array2.getCapacity());
+        System.out.println(array2.getSize());
+        System.out.println(array2.isEmpty());
+    }
+}
---
+++ src/a_Array/f_Generic_Data_Structures/Main.java
@@ -0,0 +1,7 @@
+package a_Array.f_Generic_Data_Structures;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
